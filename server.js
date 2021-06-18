ff771570410a12e36973723a93767530f6d702e6
Saketha Ramanjam <ramanujamsamavedam@hotmail.com>
2021-06-17 21:26:01 -0500

    update port settings on server.js

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get("/", (request, response) => {
 
 
 // listen for requests :)
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
