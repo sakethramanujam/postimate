cae16ee14d47091a32600986bfbbe411bd6d4baa
sakethramanujam <ramanujamsamavedam@hotmail.com>
2021-06-30 12:13:31 -0700

    [refactor] convert to arrow functions on idb promises

--- public/js/calibrate.js
+++ public/js/calibrate.js
@@ -1,13 +1,13 @@
 let positions = [];
 let clickCounter = 0;
-let date = new Date()
-let filename_base = String(date.getMonth()) + "_" + String(date.getDate()) 
+let date = new Date();
+let filename_base = String(date.getMonth()) + "_" + String(date.getDate());
 let webgazerCanvas = null;
 let dbName = "gaze_data";
-class database{
+class database {
   constructor(dbName) {
     this.dbName = dbName;
-    this.objStName = null;
+    this.objStName = "";
     if (dbName === "gaze_data") {
       this.objStName = "eyePos";
     } else if (dbName === "localforage") {
@@ -15,15 +15,15 @@ class database{
     }
   }
   createDB() {
-    let request = indexedDB.open(this.dbName, 1);
-    request.onerror = function (event) {
-      console.log(event);
+    let req = indexedDB.open(this.dbName, 1);
+    req.onerror = (e) => {
+      console.log(e);
     };
-    request.onsuccess = function (event) {
+    req.onsuccess = (e) => {
       console.log("Fresh indexDB created.");
     };
-    request.onupgradeneeded = function (event) {
-      let db = event.target.result;
+    req.onupgradeneeded = (e) => {
+      let db = e.target.result;
       if (!db.objectStoreNames.contains(this.objStName)) {
         let objectStore = db.createObjectStore(this.objStName, {
           keyPath: "timestamp",
@@ -32,133 +32,148 @@ class database{
       } else {
         console.log("objectStore already present!");
       }
+      db.close();
     };
   }
 
   clearData() {
     let req = indexedDB.deleteDatabase(this.dbName);
-    req.onsuccess = function () {
+    req.onsuccess = () => {
       console.log("Deleted database successfully");
     };
-    req.onerror = function () {
+    req.onerror = () => {
       console.log("Couldn't delete database");
     };
-    req.onblocked = function () {
+    req.onblocked = () => {
       console.log(
         "Couldn't delete database due to the operation being blocked"
       );
     };
   }
-  pushData(positions){
-    let request = indexedDB.open(this.dbName, 1);
-    request.onsuccess = function(e){
-      let db = request.result;
-      let transaction = db.transaction(this.objStName,"readwrite");
+
+  pushData(positions) {
+    let req = indexedDB.open(this.dbName, 1);
+    req.onsuccess = (e) => {
+      let db = req.result;
+      let transaction = db.transaction(this.objStName, "readwrite");
       let positionStore = transaction.objectStore(this.objStName);
-        positions.forEach(function(position) {
-              positionStore.add(position);
-            });
-    }
+      positions.forEach(function (position) {
+        positionStore.add(position);
+      });
+    };
+    req.onerror = (e) => {
+      console.log(e);
+    };
   }
 
-  getData(filetype) {
-    let request = indexedDB.open(this.dbName, 1);
-    request.onsuccess = function (e) {
-      let db = request.result;
-      let txn = db.transaction(this.objStName);
+  getData(event) {
+    let srcEl = event.source || event.target;
+    let id = srcEl.getAttribute("id");
+    let filetype = "";
+    if (id === "download_json") {
+      filetype = "json";
+    } else {
+      filetype = "csv";
+    }
+    let req = indexedDB.open(this.dbName, 1);
+    req.onsuccess = (e) => {
+      let db = req.result;
+      let txn = db.transaction(this.objStName, "readonly");
       let objectStore = txn.objectStore(this.objStName);
       let dataReq = objectStore.getAll();
-      dataReq.onsuccess = function (e) {
+      dataReq.onsuccess = (e) => {
         let jsonData = dataReq.result;
-        exportData(filetype,jsonData);
+        exportData(filetype, jsonData);
       };
-      dataReq.onerror = function (e) {
+      dataReq.onerror = (e) => {
         console.log("fetching data from indexDB failed;");
-      };  
+      };
     };
   }
 }
 
-function exportData(filetype, jsonData){
-    if (filetype === "json") {
-      _exportToJsonFile(jsonData);
-    } else {
-      _exportToCsvFile(jsonData);
-    }
+function exportData(filetype, jsonData) {
+  if (filetype === "json") {
+    _exportToJsonFile(jsonData);
+  } else {
+    _exportToCsvFile(jsonData);
+  }
 }
 
 function _exportToJsonFile(jsonData) {
   let dataStr = JSON.stringify(jsonData);
-  let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+  let dataUri =
+    "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
 
-  let exportFileDefaultName = "experiment_"+filename_base+".json";
+  let exportFileDefaultName = "experiment_" + filename_base + ".json";
 
-  let linkElement = document.createElement('a');
-  linkElement.setAttribute('href', dataUri);
-  linkElement.setAttribute('download', exportFileDefaultName);
+  let linkElement = document.createElement("a");
+  linkElement.setAttribute("href", dataUri);
+  linkElement.setAttribute("download", exportFileDefaultName);
   linkElement.click();
-  console.log("Saving "+jsonData.length+" datapoints to " + exportFileDefaultName);
+  console.log(
+    "Saving " + jsonData.length + " datapoints to " + exportFileDefaultName
+  );
 }
 
 function _parseJSONToCSVStr(jsonData) {
-  if(jsonData.length == 0) {
-      return '';
+  if (jsonData.length == 0) {
+    return "";
   }
 
   let keys = Object.keys(jsonData[0]);
 
-  let columnDelimiter = ',';
-  let lineDelimiter = '\n';
+  let columnDelimiter = ",";
+  let lineDelimiter = "\n";
 
   let csvColumnHeader = keys.join(columnDelimiter);
   let csvStr = csvColumnHeader + lineDelimiter;
 
-  jsonData.forEach(item => {
-      keys.forEach((key, index) => {
-          if( (index > 0) && (index < keys.length-1) ) {
-              csvStr += columnDelimiter;
-          }
-          csvStr += item[key];
-      });
-      csvStr += lineDelimiter;
+  jsonData.forEach((item) => {
+    keys.forEach((key, index) => {
+      if (index > 0 && index < keys.length - 1) {
+        csvStr += columnDelimiter;
+      }
+      csvStr += item[key];
+    });
+    csvStr += lineDelimiter;
   });
 
-  return encodeURIComponent(csvStr);;
+  return encodeURIComponent(csvStr);
 }
 
 function _exportToCsvFile(jsonData) {
   let csvStr = this._parseJSONToCSVStr(jsonData);
-  let dataUri = 'data:text/csv;charset=utf-8,'+ csvStr;
+  let dataUri = "data:text/csv;charset=utf-8," + csvStr;
 
-  let exportFileDefaultName = "experiment_"+filename_base+".csv";
+  let exportFileDefaultName = "experiment_" + filename_base + ".csv";
 
-  let linkElement = document.createElement('a');
-  linkElement.setAttribute('href', dataUri);
-  linkElement.setAttribute('download', exportFileDefaultName);
+  let linkElement = document.createElement("a");
+  linkElement.setAttribute("href", dataUri);
+  linkElement.setAttribute("download", exportFileDefaultName);
   linkElement.click();
 }
 
 let gazedb = new database(dbName);
 
 window.saveDataAcrossSessions = false;
-window.onload = async function(){
+window.onload = async function () {
   gazedb.createDB();
-  if(!window.saveDataAcrossSessions){
+  if (!window.saveDataAcrossSessions) {
     /* let localstorageDatalabel = 'webgazerGlobalData';
     localforage.setItem(localstorageDatalabel, null);
     let localstorageSettingslabel = 'webgazerGlobalSettings';
     localforage.setItem(localstorageSettingslabel, null); */
   }
-
 };
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   if (window.saveDataAcrossSessions) {
-      webgazer.end();
+    webgazer.end();
   } else {
-      gazedb.clearData();
+    gazedb.clearData();
   }
-}
+};
 
 function coll(element) {
   if (element.value < 3) {
